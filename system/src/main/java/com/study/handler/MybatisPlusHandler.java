071834f3e24acb76b745e9007e59df9db6f1419c
yangz <3260519078@qq.com>
2023-12-20 17:18:38 +0800

    feat:新增实体类数据自动填充

---
+++ system/src/main/java/com/study/handler/MybatisPlusHandler.java
@@ -0,0 +1,27 @@
+package com.study.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+/**
+ * @author yangz
+ * @createTime 2023/12/20 - 17:05
+ */
+@Component
+public class MybatisPlusHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        //新增或修改自动填充
+        this.setFieldValByName("createTime",new Date(),metaObject);
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        //修改自动填充
+        this.setFieldValByName("updateTime",new Date(),metaObject);
+    }
+}
